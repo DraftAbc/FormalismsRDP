b8325dc436e86efa185fa762805fc265b5a06410
GhaziAbc <ghaziabcha@gmail.com>
2015-07-28 16:50:37 +0200

    Create hyper-multi-graph.lua

---
+++ hyper-multi-graph.lua
@@ -0,0 +1,49 @@
+--hyperMultiGraph
+package.path = package.path .. ";/home/Documents/CosyVerif/library/src/?.lua"
+
+local Repository = require "cosy.repository"
+local repository = Repository.new()
+
+Repository.options (repository).create = function () return {} end
+Repository.options (repository).import = function () return {} end
+
+local _ = Repository.placeholder(repository)
+
+repository.hyper_multi_graph = {
+	hyper_multi_graph_type = {
+		vertex_type = {},
+		
+		edge_type = {
+			arrow_type = {
+				value_type = place_type,
+        value_type = place_type,
+			}, 
+			arrows = {
+			  ["cosy:meta"] = {
+		      content_type = _.arrow_type,
+		      checks = {},
+  	    }
+			} --extends typed_collection with type arrow_type
+		},
+		
+		places = {
+		  ["cosy:meta"] = {
+		    content_type = _.place_type,
+		    checks = {},
+		  },
+                transitions = {
+		  ["cosy:meta"] = {
+		    content_type = _.transition_type,
+		    checks = {},
+		  },
+
+		}, 
+		edges = {
+		  ["cosy:meta"] = {
+		    content_type = _.edge_type,
+		    checks = {},
+		  },
+		} --extends typed_collection with type edge_type
+	}
+}
+return repository.hyper_multi_graph
