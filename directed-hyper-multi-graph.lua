ffe5956b3e0a51696fdec17ed0c688370ad9c700
GhaziAbc <ghaziabcha@gmail.com>
2015-07-28 16:29:10 +0200

    Create directed-hyper-multi-graph.lua

---
+++ directed-hyper-multi-graph.lua
@@ -0,0 +1,40 @@
+--directed_HyperMultiGraph
+package.path = package.path .. ";/home/Documents/CosyVerif/library/src/?.lua"
+
+local Repository = require "cosy.repository"
+local HMG = require "hyper-multi-graph"
+
+repository = Repository.new()
+Repository.options (repository).create = function () return {} end
+Repository.options (repository).import = function () return {} end
+
+repository.hyper_multi_graph = HMG
+
+local _ = Repository.placeholder(repository)
+
+repository.directed_hyper_multi_graph = {
+	[Repository.depends] = {
+		repository.hyper_multi_graph
+	},
+	
+	directed_hyper_multi_graph_type = {
+		[Repository.refines] = {
+			_.hyper_multi_graph_type
+		},
+
+		label_edge_type = {},
+		
+		edge_type = {
+		  
+		  direction_type = {},
+		  
+		  arrow_type = {
+		    direction = {},
+		  },
+		  
+    },
+	},
+	
+}
+
+return repository.directed_hyper_multi_graph
