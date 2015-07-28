e19ee6c36c19473a747f1141aa670bc0534839ab
GhaziAbc <ghaziabcha@gmail.com>
2015-07-28 16:59:39 +0200

    Create valuation.lua

---
+++ valuation.lua
@@ -0,0 +1,17 @@
+--valuation
+package.path = package.path .. ";/home/Documents/CosyVerif/library/src/?.lua"
+
+local Repository = require "cosy.repository"
+local repository = Repository.new()
+
+Repository.options (repository).create = function () return {} end
+Repository.options (repository).import = function () return {} end
+
+repository.valuation = {
+  avaluation_type = {
+    symbol_type = {},
+    symbols = {},
+  },
+}
+    
+return repository.valuation
