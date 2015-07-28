9cfc7bbd7d937242d64179233fb175555e16281b
GhaziAbc <ghaziabcha@gmail.com>
2015-07-28 16:30:57 +0200

    Create graph.lua

---
+++ graph.lua
@@ -0,0 +1,37 @@
+--graph
+package.path = package.path .. ";/home/Documents/CosyVerif/library/src/?.lua"
+
+local Repository = require "cosy.repository"
+local MG = require "multi-graph"
+local HG = require "hyper-graph"
+
+repository = Repository.new()
+Repository.options (repository).create = function () return {} end
+Repository.options (repository).import = function () return {} end
+
+repository.multi_graph = MG
+repository.hyper_graph = HG
+
+local _ = Repository.placeholder(repository)
+
+repository.graph = {
+	[Repository.depends] = {
+		repository.hyper_graph,
+		repository.multi_graph
+	},
+	
+	graph_type = {
+		[Repository.refines] = {
+			_.hyper_graph_type,
+			_.multi_graph_type
+		},
+	  
+	  places = {},
+          
+   transitions={},
+   
+	}
+}
+
+return repository.graph
+				
